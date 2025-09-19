e4a1a34bc2eb5d3c6630cef6b36e7cc320cd513b
baladi39 <gbaladi@live.com>
2025-09-19 18:02:06 +0400

    Update AssemblyInfo and related binaries for versioning

--- obj/Debug/net9.0/auth-tester.AssemblyInfo.cs
+++ obj/Debug/net9.0/auth-tester.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("auth-tester")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+965d8da765fdb8167b820219117523fdcfccbeea")]
 [assembly: System.Reflection.AssemblyProductAttribute("auth-tester")]
 [assembly: System.Reflection.AssemblyTitleAttribute("auth-tester")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
